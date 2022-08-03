821f414b7b70e6914c4ca49eeefb6286e8a0fd9c
Ohad Green <ohadgreen@Ohads-MacBook-Pro.local>
2022-08-03 16:58:27 +0300

    organized imports

--- __tests__/Task.test.tsx
+++ __tests__/Task.test.tsx
@@ -1,13 +1,9 @@
 import { TaskStatusType } from "@/constants/task_status";
 import Task from "@/core/components/Task/Task";
-import { useTasks } from "@/core/components/Tasks/hooks/useTasks";
-import store from "@/store/store";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-import AppTestUtil from "../app_test.util";
 import { TaskProps } from "interfaces/task_props.interface";
-import { Provider } from "react-redux";
-import { createTask } from "@/store/actions/tasks.actions";
+import AppTestUtil from "../app_test.util";
 
 describe("Testing task component", () => {
   it("Testing task component showing on screen", () => {
