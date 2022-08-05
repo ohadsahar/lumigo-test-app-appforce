d00a7bdcb67e6cd5ea61d1d4c3f0fd7082b28b4c
Ohad Green <ohadgreen@Ohads-MacBook-Pro.local>
2022-08-05 15:31:25 +0300

    removed useMemo because it doesn't calculate when changing status

--- core/components/ProgressLine/hooks/useProgressLine.ts
+++ core/components/ProgressLine/hooks/useProgressLine.ts
@@ -8,25 +8,15 @@ import { useSelector } from "react-redux";
 export const useProgressLine = () => {
   const { tasks } = useSelector((state: RootState) => state.taskState);
 
-  const tasksPercentage = useMemo(
-    () =>
-      tasks?.filter((task: TaskProps) => task.status === TaskStatusType.CREATED)
-        .length ?? 0,
-    [tasks]
-  );
-  const toDoTasksPercentage = useMemo(
-    () =>
-      tasks?.filter((task: TaskProps) => task.status === TaskStatusType.PENDING)
-        .length ?? 0,
-    [tasks]
-  );
-  const completedTasksPercentage = useMemo(
-    () =>
-      tasks?.filter(
-        (task: TaskProps) => task.status === TaskStatusType.COMPLETED
-      ).length ?? 0,
-    [tasks]
-  );
+  const tasksPercentage =
+    tasks?.filter((task: TaskProps) => task.status === TaskStatusType.CREATED)
+      .length ?? 0;
+  const toDoTasksPercentage =
+    tasks?.filter((task: TaskProps) => task.status === TaskStatusType.PENDING)
+      .length ?? 0;
+  const completedTasksPercentage =
+    tasks?.filter((task: TaskProps) => task.status === TaskStatusType.COMPLETED)
+      .length ?? 0;
   const totalTasks =
     tasksPercentage + toDoTasksPercentage + completedTasksPercentage;
 
